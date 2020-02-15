5256f5717c038c8ff12d063326a2321521148818
GangjunLee <me@gangjun.dev>
2020-02-15 21:49:59 +0900

    add auth code

--- lib/screen/wrapper.dart
+++ lib/screen/wrapper.dart
@@ -1,9 +1,20 @@
+import 'package:discuss/module/user.dart';
+import 'package:discuss/screen/auth/login.dart';
 import 'package:discuss/screen/home/Home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+
+    final user = Provider.of<User>(context);
+
+    if(user == null){
+      return Login();
+    }else{
+      return Home();
+    }
+
   }
 }
