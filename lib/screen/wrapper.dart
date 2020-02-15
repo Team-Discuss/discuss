01b08c9327b755db76152085b6cab0d367e34687
GangjunLee <me@gangjun.dev>
2020-02-15 20:52:28 +0900

    make app structure

---
+++ lib/screen/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:discuss/screen/home/Home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
